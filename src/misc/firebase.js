9d5fb4f2b1e6e7fbcd2d07fcda015cf72c0031ac
diuere <86838002+diuere@users.noreply.github.com>
2023-03-19 02:13:43 -0300

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC_U66rAtGaGdpjmQiidqVOW6e8RX1X8Pw',
+  authDomain: 'chat-web-app-cd533.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-cd533-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-cd533',
+  storageBucket: 'chat-web-app-cd533.appspot.com',
+  messagingSenderId: '627621238131',
+  appId: '1:627621238131:web:26c4059c213e86a6c3a106',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+
