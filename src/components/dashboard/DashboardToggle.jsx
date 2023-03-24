8ca9579e15ba31e17f0d8e3cedc40ec4aa6cef07
diuere <86838002+diuere@users.noreply.github.com>
2023-03-24 02:34:30 -0300

    drawer responsiveness changed
    >> useMediaQuery hook created;
    Drawer is now responsive.

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,17 +1,18 @@
 import { Button, Drawer } from 'rsuite';
 import { Dashboard } from '@rsuite/icons';
-import { useModalState } from '../../helpers/custom-hooks';
+import { useMediaQuery, useModalState } from '../../helpers/custom-hooks';
 import DashboardIndex from '.'
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Dashboard icon="dashboard" /> Dashboard
       </Button>
-      <Drawer open={isOpen} onClose={close} placement="left">
+      <Drawer size={isMobile ? "full" : "sm"} open={isOpen} onClose={close} placement="left">
         <DashboardIndex />
       </Drawer>
     </>
--- src/helpers/custom-hooks.jsx
+++ src/helpers/custom-hooks.jsx
@@ -1,11 +1,29 @@
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 
 export const useModalState = (defaultVal = false) => {
+  // shall determine the dashboard brawer open state
   const [isOpen, setIsOpen] = useState(defaultVal);
 
   const open = useCallback(() => setIsOpen(true), []);
   const close = useCallback(() => setIsOpen(false), []);
 
   return { isOpen, open, close };
+}
+
+export const useMediaQuery = (query) => {
+  // shall allow the manipulation of media queries programmatically
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+
+    const handleChange = e => setMatches(e.matches)
+
+    queryList.addEventListener("change", handleChange);
+
+    return () => queryList.removeEventListener("change", handleChange);
+  }, [query])
+
+  return matches;
 }
