daa9d99d282f9ca8241b76343b0459bcd9a90195
diuere <86838002+diuere@users.noreply.github.com>
2023-03-25 05:59:18 -0300

    sign out functionality implemented

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,11 +1,24 @@
-import { Button, Drawer } from 'rsuite';
+import { Button, Drawer, Notification, toaster } from 'rsuite';
 import { Dashboard } from '@rsuite/icons';
 import { useMediaQuery, useModalState } from '../../helpers/custom-hooks';
 import DashboardIndex from '.'
+import { auth } from '../../misc/firebase';
+import { useCallback } from 'react';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width: 768px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+
+    toaster.push(
+      <Notification type="info" header="info" />,
+      { placement: 'topStart', duration: 4000 }
+    );
+    
+    close();
+  }, [close])
 
   return (
     <>
@@ -13,7 +26,8 @@ const DashboardToggle = () => {
         <Dashboard icon="dashboard" /> Dashboard
       </Button>
       <Drawer size={isMobile ? "full" : "sm"} open={isOpen} onClose={close} placement="left">
-        <DashboardIndex />
+        <DashboardIndex onSignOut={onSignOut} />
+        
       </Drawer>
     </>
   );
--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,11 +1,25 @@
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile-context';
 
+const DashboardIndex = ({ onSignOut }) => {
+  const { profile } = useProfile();
 
-const DashboardIndex = () => {
   return (
-    <div>
-      hello world
-    </div>
-  )
-}
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+        <Drawer.Actions>
+          <Button block color="red" appearance="primary" onClick={onSignOut}>
+            Sign out
+          </Button>
+        </Drawer.Actions>
+      </Drawer.Header>
 
-export default DashboardIndex
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+    </>
+  );
+};
+
+export default DashboardIndex;
