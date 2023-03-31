11462c20ad06f5034bfd01fbf6689dbb2dfd7d3d
diuere <86838002+diuere@users.noreply.github.com>
2023-03-31 02:13:29 -0300

    chat messages update
    * messages are obtain from the database;
    * messages component created;
    * messages can be displayed;
    * users profile modal created.

---
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -0,0 +1,26 @@
+import TimeAgo from 'react-timeago';
+import ProfileAvatar from '../../dashboard/ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+
+const MessageItem = ({ message }) => {
+  const { author, createdAt, text } = message;
+  return (
+    <li className='mt-2'>
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+        <ProfileInfoBtnModal profile={author} />
+        <TimeAgo date={createdAt} className="font-normal text-black-45 ml-2" />
+      </div>
+      <div className="ml-1">
+        <span className="word-break-all">{text}</span>
+      </div>
+    </li>
+  );
+};
+
+export default MessageItem;
--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,9 +1,41 @@
-
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { transformToArrayWithId } from '../../../helpers/utils';
+import { database } from '../../../misc/firebase';
+import MessageItem from './MessageItem';
 
 const ChatMessages = () => {
-  return (
-    <div>ChatMessages</div>
-  )
-}
+  const [messages, setMessages] = useState(null);
+  const { chatId } = useParams();
 
-export default ChatMessages
+  useEffect(() => {
+    const messagesRef = database.ref('/messages');
+
+    // setting a listener to messages that are related to the specific room
+    messagesRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformToArrayWithId(snap.val());
+
+        setMessages(data);
+      });
+
+    return () => {
+      messagesRef.off('value');
+    };
+  }, [chatId]);
+
+  // declaring the conditions for rendering
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No messages yet</li>}
+      {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
+};
+
+export default ChatMessages;
