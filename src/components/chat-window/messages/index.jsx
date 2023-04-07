644bbc43d25e593e524ebb4b48f00a4ae9fea353
diuere <86838002+diuere@users.noreply.github.com>
2023-04-07 02:41:08 -0300

    group chat feed by dates update
    * new helper function created;
    * messages elements are subsequently accompanied by li elements which display the grouping date and act as a separator for the messages.

--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toggleToasterPush } from '../../../helpers/custom-hooks';
-import { transformToArrayWithId } from '../../../helpers/utils';
+import { groupByDate, transformToArrayWithId } from '../../../helpers/utils';
 import { database, storage } from '../../../misc/firebase';
 import MessageItem from './MessageItem';
 
@@ -17,9 +17,9 @@ const ChatMessages = () => {
       .orderByChild('roomId')
       .equalTo(chatId)
       .on('value', snap => {
-        if(snap.val()) {
+        if (snap.val()) {
           const data = transformToArrayWithId(snap.val());
-          setMessages(data)
+          setMessages(data);
         }
       });
 
@@ -39,72 +39,105 @@ const ChatMessages = () => {
           if (admins[uid]) {
             // if the user is already an admin, revoke the permission
             admins[uid] = null;
-            alertMsg = "Admin permission removed";
+            alertMsg = 'Admin permission removed';
           } else {
             // else, give the permission
             admins[uid] = true;
-            alertMsg = "Admin permission granted";
-          } 
+            alertMsg = 'Admin permission granted';
+          }
         }
         return admins;
       });
 
-      toggleToasterPush("info", "Info", `${alertMsg}`, "topCenter", 2000);
+      toggleToasterPush('info', 'Info', `${alertMsg}`, 'topCenter', 2000);
     },
     [chatId]
   );
 
-  const handleMsgDelete = useCallback(async (messageId, file) => {
-    const confirm = window.confirm(`Are you sure you want to delete this message?`);
-    if (!confirm) return;
+  const handleMsgDelete = useCallback(
+    async (messageId, file) => {
+      const confirm = window.confirm(
+        `Are you sure you want to delete this message?`
+      );
+      if (!confirm) return;
 
-    // creating a new messages object for the database update
-    const updates = {};
+      // creating a new messages object for the database update
+      const updates = {};
 
-    updates[`/messages/${messageId}`] = null; // removing the message selected from it
+      updates[`/messages/${messageId}`] = null; // removing the message selected from it
 
-    // determining if the message selected is also the last message sent to the chat
-    const isLast = messages[messages.length - 1].id === messageId;  
+      // determining if the message selected is also the last message sent to the chat
+      const isLast = messages[messages.length - 1].id === messageId;
 
-    if (isLast) {
-      if (messages.length > 1){
-        // reassigning the last message to its predecessor
-        updates[`/rooms/${chatId}/lastMessage`] = { 
-          ...messages[messages.length - 2],
-          msgId: messages[messages.length - 2].id
-        };
-      } else {
-        updates[`/rooms/${chatId}/lastMessage`] = null;
+      if (isLast) {
+        if (messages.length > 1) {
+          // reassigning the last message to its predecessor
+          updates[`/rooms/${chatId}/lastMessage`] = {
+            ...messages[messages.length - 2],
+            msgId: messages[messages.length - 2].id,
+          };
+        } else {
+          updates[`/rooms/${chatId}/lastMessage`] = null;
+        }
       }
-    }
 
-    try {
-      await database.ref().update(updates);
-    } catch (error) {
-      toggleToasterPush("error", "Error", `${error.message}`, "topCenter", 4000);
-      return;
-    }
-
-    // handling the delete of files on the firebase storage
-    if (file) {
       try {
-        const fileRef = storage.refFromURL(file.url);
-        await fileRef.delete();
+        await database.ref().update(updates);
       } catch (error) {
-        toggleToasterPush("error", "Error", `${error.message}`, "topCenter", 4000);
+        toggleToasterPush('error', 'Error', `${error.message}`, 'topCenter', 4000);
+        return;
       }
-    }
-  }, [chatId, messages])
+
+      // handling the delete of files on the firebase storage
+      if (file) {
+        try {
+          const fileRef = storage.refFromURL(file.url);
+          await fileRef.delete();
+        } catch (error) {
+          toggleToasterPush('error', 'Error', `${error.message}`, 'topCenter', 4000);
+        }
+      }
+    },
+    [chatId, messages]
+  );
 
   // declaring the conditions for rendering
-  const isChatEmpty = !messages ||  messages.length === 0;
+  const isChatEmpty = !messages || messages.length === 0;
   const canShowMessages = messages && messages.length > 0;
+
+  const renderMessages = () => {
+    const groups = groupByDate(messages);
+    
+    const items = [];
+
+    // loop over every data and
+    Object.keys(groups).forEach(date => {
+      // render a date line element
+      items.push(
+        <li key={date} className="text-center mb-1 padded">
+          {date}
+        </li>
+      );
+      // create messages array related to this current date
+      const msgs = groups[date].map(msg => (
+        <MessageItem
+          key={msg.id}
+          message={msg}
+          handleAdmin={handleAdmin}
+          handleMsgDelete={handleMsgDelete}
+        />
+      ));
+
+      items.push(...msgs);
+    });
+
+    return items;
+  };
 
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No messages yet</li>}
-      {canShowMessages &&
-        messages.map(msg => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleMsgDelete={handleMsgDelete} />)}
+      {canShowMessages && renderMessages()}
     </ul>
   );
 };
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -30,20 +30,36 @@ export async function getUserUpdates(userId, keyToUpdate, value, database) {
     .equalTo(userId)
     .once('value');
 
-    // resolving promises
-    const [messagesSnap, roomsSnap] = await Promise.all([getMsgs, getRooms]);
+  // resolving promises
+  const [messagesSnap, roomsSnap] = await Promise.all([getMsgs, getRooms]);
 
-    // handling messages and rooms prop update
-    messagesSnap.forEach(msgSnap => {
-      updates[`/messages/${msgSnap.key}/author/${keyToUpdate}`] = value;
-    })
-    roomsSnap.forEach(rSnap => {
-      updates[`/rooms/${rSnap.key}/lastMessage/author/${keyToUpdate}`] = value;
-    })
+  // handling messages and rooms prop update
+  messagesSnap.forEach(msgSnap => {
+    updates[`/messages/${msgSnap.key}/author/${keyToUpdate}`] = value;
+  });
+  roomsSnap.forEach(rSnap => {
+    updates[`/rooms/${rSnap.key}/lastMessage/author/${keyToUpdate}`] = value;
+  });
 
-    return updates;
+  return updates;
 }
 
 export function transformToArray(snapVal) {
   return snapVal ? Object.keys(snapVal) : [];
+}
+
+export function groupByDate(messages) {
+  // returns an object with keys as dates and values as arrays of messages for each date by
+  return messages.reduce((result, message) => {
+    // creating a grouping key for each message by converting its createdAt property to a date string 
+    const groupingKey = new Date(message.createdAt).toDateString();
+
+    // and if the grouping key doesn't exist in the accumulator object, add it with an empty array value
+    if (!result[groupingKey]) result[groupingKey] = [];
+
+    // then pushing the message to the array associated with its grouping key in the accumulator object
+    result[groupingKey].push(message);
+
+    return result;
+  }, {});
 }
